49203cafe2cc91cf625a0e96b3198c219d46932d
Muhamad Arif Md Basri <4927483+arif-basri@users.noreply.github.com>
2021-01-10 00:51:56 +0800

    Added find1 Find column index based on header name

---
+++ src/main/java/reading/find1.java
@@ -0,0 +1,98 @@
+package reading;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import common.setup;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+//find column index by header name
+public class find1 {
+    private static Sheets service;
+    private static List<List<Object>> values = new ArrayList<List<Object>>();
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        init();
+    }
+
+    private static void init() throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final String spreadsheetId = "1LjCBvNAxCm4d9sUbi8ssjc1-Mi5MismbamebjYWqE08"; //PHSP OP
+        final String range = "Sheet1!1:1";
+
+        Credential credential = setup.getCredentials();
+        service = setup.getSheetsService(credential);
+
+        getValues(spreadsheetId, range, service);
+    }
+
+    private static void getValues(String spreadsheetId, String range, Sheets service) throws IOException {
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        values = response.getValues();
+
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            processValues();
+        }
+    }
+
+    private static void processValues() {
+        int rowIdx = 0;
+        ArrayList<String> headerColumnName = new ArrayList<String>();
+
+        for (List<Object> row : values) {
+            rowIdx = processRow(rowIdx, headerColumnName, row);
+        }
+    }
+
+    private static int processRow(int rowIdx, ArrayList<String> headerColumnName, List<Object> row) {
+        rowIdx++;
+        System.out.println("Reading Row: "+rowIdx+" out of : "+values.size());
+        int columnCount = row.size();
+//                int columnCount =7;  //<-- you will get indexOutOfBoundException if fixed the column count to read
+        Object valueCol=null;
+        ArrayList<String> valueLn = new ArrayList<String>();
+
+        for (int col = 0; col < columnCount; col++) {
+
+            valueCol = processColumn(rowIdx, headerColumnName, row, columnCount, valueCol, valueLn, col);
+        }
+
+        System.out.println("Output Row: "+rowIdx+ " : " + valueLn);
+        int idColumnIdx=valueLn.indexOf("__PowerAppsId__");
+        System.out.println("Index of __PowerAppsId__ column: "+ idColumnIdx);
+        return rowIdx;
+    }
+
+    private static Object processColumn(int rowIdx, ArrayList<String> headerColumnName, List<Object> row, int columnCount, Object valueCol, ArrayList<String> valueLn, int col) {
+        if (rowIdx==1) {
+            headerColumnName.add(row.get(col).toString());
+            System.out.println("Reading header column: "+col +" : "+headerColumnName.get(col));
+        } else {
+            System.out.println("Reading data columns: "+(col+1)+" out of : "+columnCount+" Name: "+headerColumnName.get(col));
+        }
+
+        if(row.get(col)!=null){
+//                        System.out.println("getting value " + col);
+            valueCol = row.get(col);//.toString();
+//                        System.out.println("got value "+ col);
+        }
+
+        if (valueCol == null||valueCol.toString().isEmpty()){
+//                        System.out.println("null value");
+            valueLn.add(col, "<null>");
+        } else {
+//                        System.out.println("value : "+valueCol.toString());
+            valueLn.add(col, valueCol.toString());
+        }
+        return valueCol;
+    }
+}
