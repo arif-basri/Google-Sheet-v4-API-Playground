59424ddb296f4a6b5770394dc0fda01f7ea7babb
Muhamad Arif Md Basri <4927483+arif-basri@users.noreply.github.com>
2021-01-09 11:53:24 +0800

    Refine google api reading shit code 2

--- src/main/java/reading/read2.java
+++ src/main/java/reading/read2.java
@@ -31,14 +31,18 @@ public class read2 {
         if (values == null || values.isEmpty()) {
             System.out.println("No data found.");
         } else {
-
+            int rowIdx = 0;
             for (List<Object> row : values) {
+                rowIdx++;
+                System.out.println("Reading Row: "+rowIdx+" out of : "+values.size());
                 int columnCount = row.size();
 //                int columnCount =7;  //<-- you will get indexOutOfBoundException if fixed the column count to read
                 Object valueCol=null;
                 ArrayList<String> valueLn = new ArrayList<String>();
 
                 for (int col = 0; col < columnCount-1; col++) {
+                    System.out.println("Reading columns: "+(col+1)+" out of : "+columnCount);
+
                     if(row.get(col)!=null){
 //                        System.out.println("getting value " + col);
                         valueCol = row.get(col);//.toString();
@@ -48,8 +52,7 @@ public class read2 {
                     if (valueCol == null||valueCol.toString().isEmpty()){
 //                        System.out.println("null value");
                         valueLn.add(col, "<null>");
-                    }
-                    else {
+                    } else {
 //                        System.out.println("value : "+valueCol.toString());
                         valueLn.add(col, valueCol.toString());
                     }
