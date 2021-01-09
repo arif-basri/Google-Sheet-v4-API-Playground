68528692ef071fc415cc494d5346986daec649f9
Muhamad Arif Md Basri <4927483+arif-basri@users.noreply.github.com>
2021-01-09 12:36:07 +0800

    Refine google api reading shit code 2 Refactor to processRow and processColumn

--- src/main/java/reading/read2.java
+++ src/main/java/reading/read2.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class read2 {
 
+    private static List<List<Object>> values = new ArrayList<List<Object>>();
     public static void main(String... args) throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final String spreadsheetId = "1LjCBvNAxCm4d9sUbi8ssjc1-Mi5MismbamebjYWqE08"; //PHSP OP
@@ -26,49 +27,62 @@ public class read2 {
         ValueRange response = service.spreadsheets().values()
                 .get(spreadsheetId, range)
                 .execute();
-        List<List<Object>> values = response.getValues();
+        values = response.getValues();
 
         if (values == null || values.isEmpty()) {
             System.out.println("No data found.");
         } else {
-            int rowIdx = 0;
-            ArrayList<String> headerColumnName = new ArrayList<String>();
-            for (List<Object> row : values) {
-                rowIdx++;
-                System.out.println("Reading Row: "+rowIdx+" out of : "+values.size());
-                int columnCount = row.size();
-
-//                int columnCount =7;  //<-- you will get indexOutOfBoundException if fixed the column count to read
-                Object valueCol=null;
-                ArrayList<String> valueLn = new ArrayList<String>();
-
-                for (int col = 0; col < columnCount; col++) {
-
-                    if (rowIdx==1) {
-                        headerColumnName.add(row.get(col).toString());
-                        System.out.println("Storing header column: "+col +" : "+headerColumnName.get(col));
-                    } else {
-                        System.out.println("Reading columns: "+(col+1)+" out of : "+columnCount+" Name: "+headerColumnName.get(col));
-                    }
-
-                    if(row.get(col)!=null){
-//                        System.out.println("getting value " + col);
-                        valueCol = row.get(col);//.toString();
-//                        System.out.println("got value "+ col);
-                    }
-
-                    if (valueCol == null||valueCol.toString().isEmpty()){
-//                        System.out.println("null value");
-                        valueLn.add(col, "<null>");
-                    } else {
-//                        System.out.println("value : "+valueCol.toString());
-                        valueLn.add(col, valueCol.toString());
-                    }
-                }
-
-                System.out.println(valueLn);
-
-            }
+            processValues();
         }
+    }
+
+    private static void processValues() {
+        int rowIdx = 0;
+        ArrayList<String> headerColumnName = new ArrayList<String>();
+
+        for (List<Object> row : values) {
+            rowIdx = processRow(rowIdx, headerColumnName, row);
+        }
+    }
+
+    private static int processRow(int rowIdx, ArrayList<String> headerColumnName, List<Object> row) {
+        rowIdx++;
+        System.out.println("Reading Row: "+rowIdx+" out of : "+values.size());
+        int columnCount = row.size();
+//                int columnCount =7;  //<-- you will get indexOutOfBoundException if fixed the column count to read
+        Object valueCol=null;
+        ArrayList<String> valueLn = new ArrayList<String>();
+
+        for (int col = 0; col < columnCount; col++) {
+
+            valueCol = processColumn(rowIdx, headerColumnName, row, columnCount, valueCol, valueLn, col);
+        }
+
+        System.out.println(valueLn);
+        return rowIdx;
+    }
+
+    private static Object processColumn(int rowIdx, ArrayList<String> headerColumnName, List<Object> row, int columnCount, Object valueCol, ArrayList<String> valueLn, int col) {
+        if (rowIdx==1) {
+            headerColumnName.add(row.get(col).toString());
+            System.out.println("Storing header column: "+col +" : "+headerColumnName.get(col));
+        } else {
+            System.out.println("Reading columns: "+(col+1)+" out of : "+columnCount+" Name: "+headerColumnName.get(col));
+        }
+
+        if(row.get(col)!=null){
+//                        System.out.println("getting value " + col);
+            valueCol = row.get(col);//.toString();
+//                        System.out.println("got value "+ col);
+        }
+
+        if (valueCol == null||valueCol.toString().isEmpty()){
+//                        System.out.println("null value");
+            valueLn.add(col, "<null>");
+        } else {
+//                        System.out.println("value : "+valueCol.toString());
+            valueLn.add(col, valueCol.toString());
+        }
+        return valueCol;
     }
 }
